d9a7e29e3c07f56188001c7bc51f18eb8fcc9421
Diogo Trindade <diogotr7@gmail.com>
2021-11-16 16:47:23 +0000

    initial commit

---
+++ src/Artemis.Plugins.BuildTask/PluginCopyTask.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Build.Framework;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Artemis.Plugins.BuildTask
+{
+    public class PluginCopyTask : MSBuildTask
+    {
+        [Required]
+        public string PluginJson { get; set; }
+
+        [Required]
+        public string SourceDirectory { get; set; }
+
+        private string DestinationDirectory { get; set; }
+
+        public override bool Execute()
+        {
+            if (!File.Exists(PluginJson))
+            {
+                Log.LogError("Error: could not find plugin.json");
+
+                return false;
+            }
+
+            var pluginInfo = JsonConvert.DeserializeObject<PluginInfo>(File.ReadAllText(PluginJson));
+
+            if (pluginInfo == null)
+            {
+                Log.LogError("Error: plugin.json formatted incorrectly");
+
+                return false;
+            }
+
+            DestinationDirectory = GetDestinationDirectory(pluginInfo);
+
+            if (!Directory.Exists(DestinationDirectory))
+            {
+                Log.LogMessage("Creating plugin destination directory \'{0}\'", DestinationDirectory);
+
+                Directory.CreateDirectory(DestinationDirectory);
+            }
+
+            DirectoryCopy(SourceDirectory, DestinationDirectory);
+
+            //copy over stuff
+            return true;
+        }
+
+        private static string GetDestinationDirectory(PluginInfo info)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                return Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
+                    "Artemis",
+                    "Plugins",
+                    info.PreferredPluginDirectory);
+            }
+            else
+            {
+                //linux (and mac?)
+                return Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "Artemis",
+                    "Plugins",
+                    info.PreferredPluginDirectory);
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, true);
+            }
+
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string tempPath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, tempPath);
+            }
+        }
+    }
+}
---
+++ src/Artemis.Plugins.BuildTask/PluginInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Artemis.Plugins.BuildTask
+{
+    internal class PluginInfo
+    {
+        /// <summary>
+        ///     The plugins GUID
+        /// </summary>
+        public Guid Guid { get; set; }
+
+        /// <summary>
+        ///     The name of the plugin
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     A short description of the plugin
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the author of this plugin
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the website of this plugin or its author
+        /// </summary>
+        public Uri Website { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the repository of this plugin
+        /// </summary>
+        public Uri Repository { get; set; }
+
+        /// <summary>
+        ///     The version of the plugin
+        /// </summary>
+        public Version Version { get; set; }
+
+        /// <summary>
+        ///     The main entry DLL, should contain a class implementing Plugin
+        /// </summary>
+        public string Main { get; set; }
+
+        internal string PreferredPluginDirectory => $"{Main.Split(new string[] { ".dll" }, StringSplitOptions.None)[0].Replace("/", "").Replace("\\", "")}-{Guid.ToString().Substring(0, 8)}";
+    }
+}
